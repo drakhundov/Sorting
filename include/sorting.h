0e800852b6e441dd0b9170dbbc2bf6fa3a139b5d
Abdul Akhundzada <abdul.akhundzadeh@gmail.com>
2025-02-04 17:50:35 +0400

    Added radix sort implementation. Implemented a helper exponent-based counting sort function. Can pass -1 to radix sort, it will determine the maximum exponent.

--- include/sorting.h
+++ include/sorting.h
@@ -1,3 +1,6 @@
 #include <vector>
 
 void countingSort(std::vector<int> &, int);
+
+void radixSort(std::vector<int> &, int = -1);
+void _countingSortWithExp(std::vector<int> &, int);  // Helper for radix sort.
